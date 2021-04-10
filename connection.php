9000b752311066bdd1da80ef82efc05f7da2b85b
lacorrales <72960041+lacorrales@users.noreply.github.com>
2021-04-09 19:19:38 -0700

    Add files via upload

--- connection.php
+++ connection.php
@@ -1,7 +1,25 @@
-<?php 
+<?php
+$value =1;
 
-require_once __DIR__ . '/vendor/autoload.php';
-    $users = (new MongoDB\Client)->PSICOLOGIA->solicitud; 
+        require_once __DIR__ . '/vendor/autoload.php';
+			$postUsername = $_POST['email']; 
+			$postPassword = $_POST['pass']; 
+            $users = (new MongoDB\Client)->PSICOLOGIA->usuarios; 
 
 
-?>
+
+            $userQuery = array('email' => $postUsername);
+            $cursor = $users->find($userQuery);
+            foreach ($cursor as $doc) {
+                $username =$doc['email'];
+                $userpass =$doc['password'];
+            }
+            if ($postUsername==$username && $postPassword == $userpass){
+                $_SESSION['authentication'] = 1;
+                $ingresa =$_SESSION;   
+                header("Location: registro.php");
+            }else{
+                    header("location: Login.php?fallo=true");
+                  }
+
+ ?> 
