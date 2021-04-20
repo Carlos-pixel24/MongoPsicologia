d0601550b1a6796defd92d9fafdc1962b10982d5
Mau_beto <75225437+Mau-beto@users.noreply.github.com>
2021-04-19 19:59:00 -0700

    Add files via upload

--- enviar.php
+++ enviar.php
@@ -1,10 +1,17 @@
 <?php
+$matricula = $_POST["matricula"];
 $nombre = $_POST["nombre"];
 $correo = $_POST["correo"];
+$grupo = $_POST["grupo"];
+$sexo = $_POST["sexo"];
+$edad = $_POST["edad"];
+$edocivil = $_POST["edocivil"];
+$tel = $_POST["telefono"];
+$carrera = $_POST["carrera"];
 $asunto = $_POST["asunto"];
 $mensaje = $_POST["mensaje"];
 
-$body = "Nombre: ". $nombre . "<br>Correo: ". $correo . "<br>Asunto: " . $asunto . "<br>Mensaje: " . $mensaje;
+$body = "Matricula: ". $matricula ."<br>Nombre: ". $nombre . "<br>Correo: ". $correo . "<br>Grupo: ". $grupo ."<br>Sexo: " .$sexo . "<br>Edad: ". $edad . "<br>Estado civil: ". $edocivil ."<br>Teléfono: ". $tel. "<br>Carrera: ". $carrera ."<br>Asunto: " . $asunto . "<br>Mensaje: " . $mensaje;
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
@@ -32,6 +39,10 @@ try {
     //Recipients
     $mail->setFrom($correo, $nombre);
     $mail->addAddress($fromemail,$fromname);     //Add a recipient
+
+
+    $mail->setFrom($fromemail, $fromname);
+    $mail->addAddress($correo,$nombre);
     
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
@@ -43,6 +54,9 @@ try {
     echo '<script>
     alert("El mensaje se envió correctamente");
     </script>';
+    
+   
+
 } catch (Exception $e) {
     echo "No se ha podido enviar el mensaje: {$mail->ErrorInfo}";
 }
