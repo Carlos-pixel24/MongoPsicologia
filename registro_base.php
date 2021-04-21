9c2554c86e0e10b878f0389fa6116c6dacf47d7e
EliLau <78751001+EliLau@users.noreply.github.com>
2021-04-21 09:37:50 -0700

    Update registro_base.php

--- registro_base.php
+++ registro_base.php
@@ -57,29 +57,25 @@ require 'Mailer/src/SMTP.php';
 $mail = new PHPMailer(true);
 
 try {
-    $fromemail = 'elii.lauma@gmail.com';
-    $fromname = 'Psicologia UTN';
+    $fromemail = 'modelodepsicologia@gmail.com';
+    $fromname = 'Psicología UTN';
     //Server settings
     $mail->SMTPDebug = 0;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = $fromemail;                     //SMTP username
-    $mail->Password   = '!E1l8i!.';                               //SMTP password
+    $mail->Password   = 'lacorrales16';                               //SMTP password
     $mail->SMTPSecure = 'tls';         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
     
     //Recipients
-    $mail->setFrom($correo, $nombre);
-    $mail->addAddress($fromemail,$fromname);     //Add a recipient
-
-
-    $mail->setFrom($fromemail, $fromname);
+    $mail->setFrom($fromemail, $fromname);      //Add a recipient
     $mail->addAddress($correo,$nombre);
     
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = $asunto;
+    $mail->Subject = 'Confirmacion Cita psicologia UTN';
     $mail->Body    = $body;
     $mail->Charset = 'UTF-8';
 
