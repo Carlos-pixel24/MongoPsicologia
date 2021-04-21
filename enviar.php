e8d5d035fab9d09dbc09ae768b5428ab72f29a8d
EliLau <78751001+EliLau@users.noreply.github.com>
2021-04-21 09:35:50 -0700

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -24,15 +24,15 @@ require 'Mailer/src/SMTP.php';
 $mail = new PHPMailer(true);
 
 try {
-    $fromemail = 'elii.lauma@gmail.com';
-    $fromname = 'Eli';
+    $fromemail = 'modelodepsicologia@gmail.com';
+    $fromname = 'Psicología UTN';
     //Server settings
     $mail->SMTPDebug = 0;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = $fromemail;                     //SMTP username
-    $mail->Password   = '!E1l8i!.';                               //SMTP password
+    $mail->Password   = 'lacorrales16';                               //SMTP password
     $mail->SMTPSecure = 'tls';         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
     
@@ -40,9 +40,6 @@ try {
     $mail->setFrom($correo, $nombre);
     $mail->addAddress($fromemail,$fromname);     //Add a recipient
 
-
-    $mail->setFrom($fromemail, $fromname);
-    $mail->addAddress($correo,$nombre);
     
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
